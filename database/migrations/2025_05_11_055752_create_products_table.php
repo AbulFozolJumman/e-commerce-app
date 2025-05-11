3a9641b5c1a352497d7f5ece8abc0e112211ade5
AbulFozolJumman <abulfozoljumman@gmail.com>
2025-05-11 19:31:26 +0600

    Wishlists table migration done

--- database/migrations/2025_05_11_055752_create_products_table.php
+++ database/migrations/2025_05_11_055752_create_products_table.php
@@ -13,29 +13,24 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-
-            // $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
-
-            // Foreign Key
-            $table->unsignedBigInteger('seller_id');
-            $table->foreign('seller_id')->references('id')->on('users')
-            ->restrictOnDelete()->cascadeOnUpdate();
+            $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
             $table->string('title');
             $table->text('description');
             $table->decimal('price', 10, 2);
             $table->string('image_url');
             $table->integer('stock');
             $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
-
-            // $table->foreignId('category_id')->constrained('categories')->onDelete('set null')->nullable();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('set null')->nullable();
             
             // Foreign Key
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('category_id')->references('id')->on('categories')
-            ->restrictOnDelete()->cascadeOnUpdate();
+            // $table->unsignedBigInteger('category_id');
+            // $table->foreign('category_id')->references('id')->on('categories')
+            // ->restrictOnDelete()->cascadeOnUpdate();
 
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->timestamps();
+
+            // $table->timestamp('created_at')->useCurrent();
+            // $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
